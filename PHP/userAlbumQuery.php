c7484e353275afee6278cdf98cc7e77d1bec2a69
Julen Ferrero <julenferre@gmail.com>
2016-12-16 11:18:00 +0100

    Datubasea

--- PHP/userAlbumQuery.php
+++ PHP/userAlbumQuery.php
@@ -6,16 +6,15 @@
 		$query = "SELECT albumak.ID, albumak.Izena, albumak.SorreraData, albumak.Egilea "
 			."FROM albumak, albumatzipenzerrenda "
 			."WHERE Egilea='".$_GET['user']."'  ";
-			if(strcmp($_GET['user'], $_SESSION['login_user'])!==0){
-				$query .= "AND "
-				."("
-					."Eskuragarritasuna = 'publikoa' "
-					."OR Eskuragarritasuna = 'atzipenMugatua' "
-					."OR (Eskuragarritasuna = 'custom' "
-						."AND albumatzipenzerrenda.AlbumID = albumak.ID "
-						."AND albumatzipenzerrenda.Nickname = '".$_SESSION['login_user']."') "
-				.") ";
-			}
+			$query .= "AND "
+			."("
+				."Eskuragarritasuna = 'publikoa' "
+				."OR Eskuragarritasuna = 'atzipenMugatua' "
+				."OR (Eskuragarritasuna = 'custom' "
+					."AND albumatzipenzerrenda.AlbumID = albumak.ID "
+					."AND albumatzipenzerrenda.Nickname = '".$_SESSION['login_user']."') "
+			.") ";
+			
 		$query .= "GROUP BY albumak.ID ORDER BY SorreraData DESC";
 	}
 	else{
