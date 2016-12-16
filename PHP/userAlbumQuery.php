0308fb61f25d78017f77738bdaa93419aff6bca1
Aimar Ugarte <ugarteaimar@gmail.com>
2016-12-16 11:12:06 +0100

    profileko albumen errorea zuzendu.

--- PHP/userAlbumQuery.php
+++ PHP/userAlbumQuery.php
@@ -2,7 +2,8 @@
 
 	include "CONNECT.php";
 
-	$query = "SELECT albumak.ID, albumak.Izena, albumak.SorreraData, albumak.Egilea "
+	if(strcmp($_GET['user'], $_SESSION['login_user'])!==0){
+		$query = "SELECT albumak.ID, albumak.Izena, albumak.SorreraData, albumak.Egilea "
 			."FROM albumak, albumatzipenzerrenda "
 			."WHERE Egilea='".$_GET['user']."'  ";
 			if(strcmp($_GET['user'], $_SESSION['login_user'])!==0){
@@ -15,7 +16,15 @@
 						."AND albumatzipenzerrenda.Nickname = '".$_SESSION['login_user']."') "
 				.") ";
 			}
-	$query .= "GROUP BY albumak.ID ORDER BY SorreraData DESC";
+		$query .= "GROUP BY albumak.ID ORDER BY SorreraData DESC";
+	}
+	else{
+		$query = "SELECT albumak.ID, albumak.Izena, albumak.SorreraData, albumak.Egilea "
+			."FROM albumak "
+			."WHERE Egilea='".$_GET['user']."'  ";
+		$query .= "GROUP BY albumak.ID ORDER BY SorreraData DESC";
 
+	}
+	
 	$erantzuna = $conn->query($query);
 ?>
